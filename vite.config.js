6731f0ea29720ce89c0378e78f2c351a89e07f96
KathrinKath <kathrin.peled@dci-student.org>
2024-10-09 20:00:32 +0200

    upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/LanguageSettings/', // Set the base URL for GitHub Pages
+})
